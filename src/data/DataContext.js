729ca15b00461f4cde256a16e98d910735b69da4
brunobandelli <brunobandelli@gmail.com>
2021-10-09 20:56:47 -0300

    useContext Part 1 Ok

---
+++ src/data/DataContext.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const data = {
+    number: 123,
+    text: 'Context API...'
+}
+
+const DataContext = React.createContext(data)
+
+export default DataContext
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -5,15 +5,22 @@ import { BrowserRouter as Router } from  'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
+import DataContext, { data } from '../data/DataContext'
+import { useState } from 'react/cjs/react.development'
+
 const App = props => {
+    const [state, setState] = useState(data)
 
     return (
+
+        <DataContext.Provider value={{state, setState}}>
                 <div className="App">
                     <Router>
                         <Menu />
                         <Content />
                     </Router>
                 </div>
+        </DataContext.Provider>
     )
 }
 
