fa87f4de1cbe2e36fd128bf9518f0638fde2f1e6
brunobandelli <brunobandelli@gmail.com>
2021-10-12 21:13:49 -0300

    Desafio useReducer Ok

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -11,7 +11,15 @@ const initialState = {
 function reducer(state, action){
     switch(action.type){
         case 'numberAdd2':
-            return {...state, number: state.number + 2}
+            return {...state, number: state.number + 2 }
+        case 'numberMulti7':
+            return { ...state, number: state.number * 7 }
+        case 'numberDiv25':
+            return { ...state, number: state.number / 25 }
+        case 'numberInt':
+            return { ...state, number: parseInt(state.number)}
+        case 'numberAddN':
+            return { ...state, number: state.number + action.payload}
         case 'login':
             return {...state, user: {name: action.payload}}
         default:
@@ -34,8 +42,13 @@ const UseReducer = (props) => {
                 : <span className="text">Sem Usuário</span> }
                 <span className="text">{state.number}</span>
                 <div>
-                    <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Maria'})}>Login</button>
+                    <button className="btn" onClick={() => dispatch({type: 'login', payload: 'Bruno'})}>Login</button>
                     <button className="btn" onClick={() => dispatch({type: 'numberAdd2'})}>+2</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberMulti7'})}>*7</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberDiv25'})}>/25</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberInt'})}>Int</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberAddN', payload: -9})}>-9</button>
+                    <button className="btn" onClick={() => dispatch({type: 'numberAddN', payload: 11})}>+11</button>
                 </div>
             </div>
 
